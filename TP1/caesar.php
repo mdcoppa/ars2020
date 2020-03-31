c2a70dc843cb907ed5771ae14ba1f9fbf09bdd1c
Mauricio Coppa <mdcoppa@gmail.com>
2020-03-31 19:36:47 -0300

    mm

---
+++ TP1/caesar.php
@@ -0,0 +1,14 @@
+<html>
+   <head>
+   </head>
+   <body>
+       <form action="metodos_encript.php" method="POST">
+          Mensaje: <input type="text" name="mensaje" style="width:385px;"/>
+          <br>
+          Desplazamiento: <input type="text" name="desplazamiento" style=" width:385px;"/>
+          <br>
+          <br>
+          <input type="submit" value="Encriptar" style="width:180px;"/>
+        </form>
+    </body>
+</html>
---
+++ TP1/metodos_encript.php
@@ -0,0 +1,45 @@
+<?php
+$mensaje = $_POST['mensaje'];
+$mensaje = strtoupper($mensaje);
+$tamano =  strlen($mensaje);
+$desplazamiento = $_POST['desplazamiento'];
+
+echo "MENSAJE A ENCRIPTAR:";
+echo "</br>";
+echo "<div class='a'>$mensaje</div>";
+echo "</br>";
+echo "</br>";
+echo "DESPAZAMIENTO:";
+echo "</br>";
+echo "<div class='a'>$desplazamiento</div>";
+echo "</br>";
+echo "</br>";
+echo "MENSAJE ENCRIPTADO:", "
+";
+for ($pos=0; $pos <= $tamano ; $pos++) {
+ $resultado1 = $mensaje[$pos];
+  /*if ($resultado1 == X) {
+   echo " ","<span style='color:red; font:bold 25px Arial;'>AAA</apan>";
+  }else{
+   if ($resultado1 == Y) {
+   echo " ","<span style='color:red; font:bold 25px Arial;'>B</apan>";
+   }else{
+    if ($resultado1 == Z) {
+    echo " ","<span style='color:red; font:bold 25px Arial;'>C</apan>";
+    }else{
+      */
+     for ($i=65;$i<=90;$i++) {
+      $letra = chr($i);
+      if ($resultado1 == $letra) {
+       $i = $i + 3;
+       $resultado =chr($i);
+       echo " ", "<span style='color:red; font:bold 25px Arial;'>$resultado</apan>";
+      }               
+     }
+    //}
+   }
+  }
+}
+echo "</br>","</br>","</br>";
+echo "<a href='caesar.php'>Atras<a/>"; 
+?>
